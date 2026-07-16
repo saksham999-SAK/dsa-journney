7c0efe982ae796dd6a2bcc99f458a19b9b870583
saksham999-SAK <sakshamboldhan9@gmail.com>
2026-07-16 13:24:20 +0530

    Time: 2 ms (72.87%) | Memory: 42.7 MB (79.90%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        if (s.length() > t.length()) {
+            return false;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        if (i == s.length()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
